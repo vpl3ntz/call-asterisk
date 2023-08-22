fe782bd3ce6a7a68d28eb7195a7639e8454c42c9
vplentz <vitor@brasilfone.com.br>
2023-08-22 13:31:30 -0300

    refactor(API): refactor

--- app/Http/Controllers/ClicktocallController.php
+++ app/Http/Controllers/ClicktocallController.php
@@ -11,22 +11,29 @@ use PAMI\Message\Action\LogoffAction;
 
 class ClicktocallController extends Controller
 {
-    private const PAMI_CLIENT_OPTIONS  = [
+    private const PAMI_CLIENT_OPTIONS = [
         'host' => '127.0.0.1',
         'scheme' => 'tcp://',
         'port' => '5038',
-        'username' => 'vpbrasilfone' ,
+        'username' => 'vpbrasilfone',
         'secret' => 'vpbsf',
         'connect_timeout' => 60000,
         'read_timeout' => 60000
     ];
+
+    /**
+     * Received the number and config to call.
+     * @param $extension
+     * @param $number
+     * @return string
+     */
     public function connection($extension, $number): string
     {
         try {
             $connection = new ClientImpl(self::PAMI_CLIENT_OPTIONS);
             $connection->open();
 
-            $action = new OriginateAction('SIP/'.$extension.'/'.$number);
+            $action = new OriginateAction('SIP/' . $extension . '/' . $number);
             $action->setContext('kamailio');
             $action->setExtension($number);
             $action->setPriority('1');
